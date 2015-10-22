11e6b9f2dd1a5c6e777fadcffb7f6e226355e653
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2015-10-22 13:38:03 +0200

    Add package.js

---
+++ package.js
@@ -0,0 +1,30 @@
+Package.describe({
+  documentation: "README.md",
+  git: "https://github.com/hansoft/meteor-one-account.git",
+  name: "useraccounts:one-account",
+  version: "0.1.0",
+  summary: "One account for all account services",
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom("1.2");
+
+	api.use([
+		"ecmascript",
+		"reactive-dict",
+	]);
+
+  api.use([
+    "accounts-base",
+    "underscore",
+  ], "server");
+
+	api.use([
+		"accounts-password",
+	], "server", { weak: true });
+
+  api.export("OneAccount");
+
+  api.addFiles("one-account.js");
+  api.addFiles("accounts.js", "server");
+});
