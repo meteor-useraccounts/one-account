8cacb7aa73010f2f0a5755ce0da4339be9223bfe
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2015-10-22 14:03:35 +0200

    Spaces to tabs

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
-  documentation: "README.md",
-  git: "https://github.com/hansoft/meteor-one-account.git",
-  name: "useraccounts:one-account",
-  version: "0.1.0",
-  summary: "One account for all account services",
+	documentation: "README.md",
+	git: "https://github.com/hansoft/meteor-one-account.git",
+	name: "useraccounts:one-account",
+	version: "0.1.0",
+	summary: "One account for all account services",
 });
 
 Package.onUse(function(api) {
@@ -14,17 +14,17 @@ Package.onUse(function(api) {
 		"reactive-dict",
 	]);
 
-  api.use([
-    "accounts-base",
-    "underscore",
-  ], "server");
+	api.use([
+		"accounts-base",
+		"underscore",
+	], "server");
 
 	api.use([
 		"accounts-password",
 	], "server", { weak: true });
 
-  api.export("OneAccount");
+	api.export("OneAccount");
 
-  api.addFiles("one-account.js");
-  api.addFiles("accounts.js", "server");
+	api.addFiles("one-account.js");
+	api.addFiles("accounts.js", "server");
 });
